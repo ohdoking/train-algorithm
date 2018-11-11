28031c6ef849c4c670596a5100cec0bb4fdea93f
Kenny Oh <ohdoking@gmail.com>
2018-11-11 22:48:08 +0100

    code fix and add comment more.

--- test/TestXbonacci.java
+++ test/TestXbonacci.java
@@ -21,21 +21,19 @@ public class TestXbonacci {
 
 	@Test
 	public void basicTests() {
-//		assertArrayEquals(new double[] { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 },
-//				variabonacci.xbonacci(new double[] { 1, 1 }, 10));
+		assertArrayEquals(new double[] { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 },
+				variabonacci.xbonacci(new double[] { 1, 1 }, 10));
 		assertArrayEquals(new double[] { 0, 0, 0, 0, 1, 1, 2, 4, 8, 16 },
 				variabonacci.xbonacci(new double[] { 0, 0, 0, 0, 1 }, 10));
-		/*
-		 * 
 		assertArrayEquals(new double[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 },
 				variabonacci.xbonacci(new double[] { 0, 1 }, 10));
 		assertArrayEquals(new double[] { 1, 0, 0, 0, 0, 0, 1, 2, 3, 6 },
-				variabonacci.xbonacci(new double[] { 1, 0, 0, 0, 0, 0, 1 }, 10));*/
+				variabonacci.xbonacci(new double[] { 1, 0, 0, 0, 0, 0, 1 }, 10));
 	}
 
 	private void assertArrayEquals(double[] expecteds, double[] actuals) {
 		for (int i = 0; i < actuals.length; i++){
-			assertEquals(expecteds[i], actuals[i], 1e-10);
+			assertEquals(expecteds[i], actuals[i], 1e-20);
 		}
 	}
 }
