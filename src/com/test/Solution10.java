773039846c451225b9e1e21c29f6a86e0b018aae
Kenny Oh <ohdoking@gmail.com>
2018-11-26 23:30:21 +0100

    add more detail comment.

--- src/com/test/Solution10.java
+++ src/com/test/Solution10.java
@@ -4,6 +4,8 @@ import java.util.stream.IntStream;
 
 /**
  * 
+ * TieRopes
+ * 
  * For a given integer K, 
  * the goal is to tie the ropes in such a way 
  * that the number of ropes whose length is greater than or equal to K is maximal.
@@ -19,11 +21,13 @@ class Solution10 {
 		//AtomicInteger count = new AtomicInteger(0);
 		
 		int count = 0;
+		//find value array over K
 		int[] standardIndexArray = IntStream
 			.range(0, A.length)
 			.filter(idx -> A[idx] >= K)
 			.toArray();
 		
+		// find value sum between standardIndexArray array
 		for(int i = 0 ; i <= standardIndexArray.length; i++) {
 			
 			int start = i == 0 ? 0 : standardIndexArray[i-1] + 1;
