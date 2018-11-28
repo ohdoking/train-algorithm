1167afdab7b5032fbe980355e22ede8e59f7ca62
Kenny Oh <ohdoking@gmail.com>
2018-11-28 01:03:38 +0100

    after add test case, fix every code. before code can't solve specific issue.

--- src/com/test/Solution11.java
+++ src/com/test/Solution11.java
@@ -4,39 +4,36 @@ package com.test;
  * 
  * MaxDoubleSliceSum
  * 
- * 	A triplet (X, Y, Z), such that 0 ≤ X < Y < Z < N, is called a double slice.
- * 	The sum of double slice (X, Y, Z) is the total of A[X + 1] + A[X + 2] + ... + A[Y − 1] + A[Y + 1] + A[Y + 2] + ... + A[Z − 1].
- * 	The goal is to find the maximal sum of any double slice.
+ * A triplet (X, Y, Z), such that 0 ≤ X < Y < Z < N, is called a double slice.
+ * The sum of double slice (X, Y, Z) is the total of A[X + 1] + A[X + 2] + ... +
+ * A[Y − 1] + A[Y + 1] + A[Y + 2] + ... + A[Z − 1]. The goal is to find the
+ * maximal sum of any double slice.
  * 
  * @author ohdoking
  *
  */
 class Solution11 {
-    public int solution(int[] A) {
-        // write your code in Java SE 8
-    	
-    	int maxEnding = 0;
-    	int maxSlice = 0;
-    	int minValue = Integer.MAX_VALUE;
-    	
-    	int sum = 0;
-    	int result = 0;
-    	for(int i = 1 ; i < A.length-1 ; i++) {
-    		sum = 0;
-    		for(int j = i ; j < A.length-1 ; j++) {
-    			sum += A[j];
-    			//minValue = minValue > A[i] ? A[i] : minValue;
-    			result = Math.max(result, sum);
-    		}
-    	}
-    	/*for(int i = 1 ; i < A.length - 1 ; i++) {
-    		maxEnding = Math.max(0, maxEnding + A[i]);
-    		minValue = minValue > A[i] ? A[i] : minValue;
-     		maxSlice = Math.max(maxSlice, maxEnding);
-    	}
-    	
-    	
-		return maxSlice + minValue ;*/
-    	return result;
-    }
+	public int solution(int[] A) {
+		int arrLength = A.length;
+		int[] tempArr = new int[arrLength];
+		int[] backTempArr = new int[arrLength];
+		int max = 0;
+
+		// define tempArr[i] as the maximum sum contiguous subsequence ending at index  i
+		for (int i = 1; i < arrLength - 1; i++) {
+			tempArr[i] = Math.max(tempArr[i - 1] + A[i], 0);
+		}
+		
+		//backTempArr[i] as the maximum sum contiguous subsequence starting with index i.
+		for (int i = arrLength - 2; i > 0; i--) {
+			backTempArr[i] = Math.max(backTempArr[i + 1] + A[i], 0);
+		}
+
+		// iterate over i, and find the maximum sum of K1[i-1]+K2[i+1]. 
+		for (int i = 1; i < arrLength - 1; i++) {
+			max = Math.max(max, tempArr[i - 1] + backTempArr[i + 1]);
+		}
+
+		return max;
+	}
 }
--- test/com/test/TestSolution11.java
+++ test/com/test/TestSolution11.java
@@ -27,9 +27,37 @@ class TestSolution11 {
 		    
 	@Test
 	void testSimple() {
-		//assertEquals(17, solution11.solution(new int[] {3, 2, 6, -1, 4, 5, -1, 2}));
+		assertEquals(17, solution11.solution(new int[] {3, 2, 6, -1, 4, 5, -1, 2}));
+		assertEquals(18, solution11.solution(new int[] {3, 2, 6, -1, 4, 5, -1, 2, 2}));
 		assertEquals(8, solution11.solution(new int[] {3, 2, 6, -1, 4}));
-		assertEquals(5, solution11.solution(new int[] {3, -1, 6, -1, 4}));
+	}
+	
+	@Test
+	void testLargeRange() {
+		int testArray[] = new int[100_000];
+		for(int i = 0 ; i < 100_000 ; i++) {
+			testArray[i] = 10_000 ;
+		}
+		assertEquals(10_000 * 100_000 - 30_000, solution11.solution(testArray));
+	}
+	
+	@Test
+	void testShortRange() {
+		assertEquals(0, solution11.solution(new int[] {3,1,-1}));
+		assertEquals(1, solution11.solution(new int[] {3,1,-10_000,-1}));
+		
+	}
+	
+	@Test
+	void testSmallValue() {
+		assertEquals(6, solution11.solution(new int[] {3, -1, 6, -1, 4}));
+		
+	}
+	
+	@Test
+	void testBigValue() {
+		assertEquals(20_000, solution11.solution(new int[] {10_000, 10_000, 10_000, -2, 10_000}));
+		
 	}
 
 }
