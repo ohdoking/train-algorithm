3726d0a2e733478baa4f5c8658faa2dd1e8b9810
Kenny Oh <ohdoking@gmail.com>
2019-01-23 00:25:44 +0100

    reference other people solution for improving my skill

---
+++ src/com/test/Solution17OtherAnswer.java
@@ -0,0 +1,31 @@
+package com.test;
+
+import java.util.HashSet;
+import java.util.Set;
+/**
+ * 
+ * @author ohdoking
+ *
+ * using sliding window 
+ * 
+ * https://www.geeksforgeeks.org/window-sliding-technique/
+ * 
+ */
+public class Solution17OtherAnswer {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            // try to extend the range [i, j]
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            }
+            else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+}
