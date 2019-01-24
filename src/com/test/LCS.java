8e482674e64ff7c826b911bb375ee14ddd36ca55
Kenny Oh <ohdoking@gmail.com>
2019-01-24 16:25:07 +0100

    solve Longest Palindromic Substring algorithm problem using DP

--- src/com/test/LCS.java
+++ src/com/test/LCS.java
@@ -1,4 +1,35 @@
 package com.test;
 
+/**
+ *
+ * LCS
+ *
+ * @author ohdoking
+ *
+ * This question is for improving algorithm solving problems using DP(Dynamic Programming).
+ *
+ *
+ *
+ */
 public class LCS {
+
+
+    public int LCSLength(String s1, String s2) {
+        int DP[][] = new int[s1.length() + 1][s2.length() + 1];
+
+
+        for(int i = 1 ; i <= s1.length(); i++){
+            for(int j = 1 ; j <= s2.length(); j++){
+                if(s1.charAt(i - 1) == s2.charAt(j - 1)){
+                    DP[i][j] = DP[i - 1][j - 1] + 1;
+                }
+                else{
+                    DP[i][j] = Math.max(DP[i - 1][j] , DP[i][j - 1]);
+                }
+            }
+        }
+
+        return DP[s1.length()][s2.length()];
+
+    }
 }
--- test/com/test/LCSTest.java
+++ test/com/test/LCSTest.java
@@ -1,4 +1,21 @@
 package com.test;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class LCSTest {
+
+    public LCS lcs;
+
+    @BeforeEach
+    public void init(){
+        lcs = new LCS();
+    }
+
+    @Test
+    public void test(){
+        assertEquals(2, lcs.LCSLength("abc", "bc"));
+    }
 }
