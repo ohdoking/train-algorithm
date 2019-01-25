f1903c7dbb7d8e4af1e6790f104405df9993cec9
Kenny Oh <ohdoking@gmail.com>
2019-01-25 23:38:50 +0100

    [fix]fix error about char array null check.

--- src/com/test/Solution19.java
+++ src/com/test/Solution19.java
@@ -47,7 +47,7 @@ class Solution19 {
 
         for(int i = 0 ; i < data.length; i++){
             for(int j = 0 ; j < data[0].length; j++){
-                if(data[i][j] != ' '){
+                if(data[i][j] != '\0'){
                     result.append(data[i][j]);
                 }
             }
