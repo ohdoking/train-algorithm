a020af6d4e95326c0e7136209425c03c8b098c7e
Kenny Oh <ohdoking@gmail.com>
2019-01-30 17:44:36 +0100

    [refactoring] optimizing to time complex from O(n^2) to O(n) using Two Pointer Approach

--- src/com/test/Solution25.java
+++ src/com/test/Solution25.java
@@ -18,7 +18,7 @@ package com.test;
  * i've just finished read this question.
  * and i came up with simple idea that
  * calculate area each every case and return the highest area.
- * at this time time complex is O(n^2) and space complex is also O(n^2)
+ * at this time time complex is O(n^2) and space complex is O(1)
  *
  * so i want to find more nice solution about this question .
  *
@@ -28,11 +28,23 @@ package com.test;
  *
  * so i need to find much better solution.
  *
+ * using the two point.
+ * i'm gonna check the each start and end point
+ * we take a two pointer, one at the beginning and one at the end of array costituting the lenght of lines.
+ * after calculating the first value and last value area then move the lower height pointer.
+ *
+ * and when the last index is less than the first index, end the while loop
  *
  *
  */
 class Solution25 {
-    public int maxArea(int[] height) {
+    /**
+     * first solution
+     *
+     * @param height
+     * @return
+     */
+    public int maxArea1(int[] height) {
         int theHighestArea = 0;
         for(int i = 0 ; i < height.length ; i++){
             for (int j = i+1 ; j < height.length; j++){
@@ -42,4 +54,28 @@ class Solution25 {
 
         return theHighestArea;
     }
+
+    /**
+     * second solution
+     */
+    public int maxArea(int[] height) {
+        int theHighestArea = 0;
+        int start = 0;
+        int last = height.length - 1;
+
+        while(start < last){
+            theHighestArea = Math.max(Math.min(height[start], height[last]) * (last - start),theHighestArea);
+            if(height[start] < height[last]){
+                start++;
+            }
+            else{
+                 last--;
+            }
+        }
+
+        return theHighestArea;
+    }
+
+
+
 }
--- test/com/test/TestSolution25.java
+++ test/com/test/TestSolution25.java
@@ -5,56 +5,48 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
 /**
  * 
- *
- * 
-this is condition.
-
-X, Y and D are integers within the range [1..1,000,000,000];
-X ≤ Y.
-
-
  * @author ohdoking
  *
  */
 class TestSolution25 {
 
-	Solution2 solution2;
+	Solution25 solution25;
 	
 	@BeforeEach
 	void setUp() throws Exception {
-		solution2 = new Solution2(); 
+		solution25 = new Solution25();
 	}
 
 	@Test
 	void testSimple() {
-		
-		assertEquals(3, solution2.solution(10, 85, 30));
-		assertEquals(90, solution2.solution(10, 100, 1));
-		
+		assertEquals(49, solution25.maxArea(new int[]{1,8,6,2,5,4,8,3,7}));
 	}
 	
 	@Test
 	void testNull() {
-		
-		assertEquals(0, solution2.solution(10, 100, 0));
-		
+		assertEquals(0, solution25.maxArea(new int[]{}));
 	}
 	
 	@Test
 	void testSame() {
-		
-		assertEquals(0, solution2.solution(10, 10, 1));
-		
+		assertEquals(13, solution25.maxArea(new int[]{1,1,1,1,1,1,1,1,1,1,1,1,1,1}));
+
 	}
 	
 	@Test
-	void testExtremePosition() {
-		
-		//extreme_position
-		assertEquals(1000000000, solution2.solution(0, 1000000000, 1));
-		
+	void testExtremeValue() {
+		int[] arr = new int[10000000];
+		for(int i = 0 ; i < 10000000; i++){
+			arr[i] = i;
+		}
+		// and timeout test
+		assertTimeout(Duration.ofMillis(1000), () -> {
+			assertEquals(2147482498, solution25.maxArea(arr));
+		});
 	}
 	
 
