7c41ed8e023d113fe6c25309707caed2fc9c0b8a
Kenny Oh <ohdoking@gmail.com>
2019-01-31 18:07:55 +0100

    implement quick sort

--- src/com/test/fundamental/QuickSort.java
+++ src/com/test/fundamental/QuickSort.java
@@ -1,4 +1,59 @@
 package com.test.fundamental;
 
-public class QuickSort {
+import java.util.Arrays;
+
+/**
+ * QuickSort
+ *
+ * @author ohdoking
+ *
+ * @param <T>
+ *
+ *     partitioning
+ */
+public class QuickSort{
+
+    public void sort(int[] arr){
+        sort(arr, 0, arr.length - 1);
+    }
+
+    private void sort(int[] arr, int start, int last){
+
+       int part = partition(arr, start ,last);
+
+       if(start < part - 1){
+           sort(arr, start, part - 1);
+       }
+
+       if(part < last){
+           sort(arr, part, last);
+       }
+    }
+
+    private int partition(int[] arr, int start, int last) {
+
+        if(last - start == 0){
+            return last;
+        }
+
+        int pivot = arr[(start+last)/2];
+
+        while(start <= last){
+            while(arr[start] < pivot){
+                start++;
+            }
+            while(arr[last] > pivot){
+                last--;
+            }
+            if(start <= last){
+                int temp = arr[last];
+                arr[last] = arr[start];
+                arr[start] = temp;
+                start++;
+                last--;
+            }
+        }
+
+        return start;
+    }
 }
--- test/com/test/fundamental/TestQuickSort.java
+++ test/com/test/fundamental/TestQuickSort.java
@@ -1,4 +1,31 @@
 package com.test.fundamental;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 public class TestQuickSort {
+
+    QuickSort quickSort;
+
+    @BeforeEach
+    void setUp(){
+        quickSort = new QuickSort();
+    }
+
+    @Test
+    void test() {
+        int[] arr = new int[] {4,5,2,3,1};
+        quickSort.sort(arr);
+        assertArrayEquals(arr, new int[] {1,2,3,4,5});
+    }
+
+    @Test
+    void testSameValue() {
+        int[] arr = new int[] {4,1,3,3,3};
+        quickSort.sort(arr);
+        assertArrayEquals(arr, new int[] {1,3,3,3,4});
+    }
+
 }
