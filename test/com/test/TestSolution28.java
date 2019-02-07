3e0d4abf6a8b2ac41166d49227fee188da711041
Kenny Oh <ohdoking@gmail.com>
2019-02-07 10:54:12 +0100

    add condition comments

--- test/com/test/TestSolution28.java
+++ test/com/test/TestSolution28.java
@@ -5,6 +5,12 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ *
+ * Given an integer, convert it to a roman numeral. Input is guaranteed to be within the range from 1 to 3999.
+ *
+ *
+ */
 public class TestSolution28 {
     Solution28 solution28;
 
