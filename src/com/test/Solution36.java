631d2454984c292dc150b61593acc8ea18531ccd
Kenny Oh <ohdoking@gmail.com>
2019-02-19 13:39:02 +0100

    solve swap nodes in pairs algorithm

---
+++ src/com/test/Solution36.java
@@ -0,0 +1,42 @@
+package com.test;
+
+import com.test.fundamental.ListNode;
+
+/**
+ *
+ * Swap Nodes in Pairs
+ *
+ * Given a linked list, swap every two adjacent nodes and return its head.
+ *
+ * You may not modify the values in the list's nodes, only nodes itself may be changed.
+ *
+ *
+ *
+ * Example:
+ *
+ * Given 1->2->3->4, you should return the list as 2->1->4->3.
+ * 
+ */
+class Solution36 {
+    public ListNode swapPairs(ListNode head) {
+        ListNode headNode = head.getNext();
+        ListNode previousNode = null;
+        while(true){
+            if(head != null && head.getNext() != null){
+                ListNode tempNode = head.getNext();
+                ListNode nextNode = head.getNext().getNext();
+                head.setNext(nextNode);
+                tempNode.setNext(head);
+                if(previousNode != null){
+                    previousNode.setNext(tempNode);
+                }
+                previousNode = head;
+                head = head.getNext();
+            }
+            else{
+                break;
+            }
+        }
+        return headNode;
+    }
+}
---
+++ test/com/test/TestSolution36.java
@@ -0,0 +1,40 @@
+package com.test;
+
+import com.test.fundamental.ListNode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestSolution36 {
+
+    Solution36 solution36;
+
+    @BeforeEach
+    void setUp() {
+        solution36 = new Solution36();
+    }
+
+    @Test
+    public void test(){
+
+        ListNode listNode = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(3);
+        ListNode listNode4 = new ListNode(4);
+        listNode3.setNext(listNode4);
+        listNode2.setNext(listNode3);
+        listNode.setNext(listNode2);
+
+        ListNode listNode1 = new ListNode(2);
+        ListNode listNode12 = new ListNode(1);
+        ListNode listNode13 = new ListNode(4);
+        ListNode listNode14 = new ListNode(3);
+        listNode13.setNext(listNode14);
+        listNode12.setNext(listNode13);
+        listNode1.setNext(listNode12);
+
+        assertEquals(listNode1, solution36.swapPairs(listNode));
+
+    }
+}
