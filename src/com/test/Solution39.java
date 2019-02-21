5efeae7acfa664783d4c2990625e9505436e543a
Kenny Oh <ohdoking@gmail.com>
2019-02-21 13:10:11 +0100

    update other solve method.

--- src/com/test/Solution39.java
+++ src/com/test/Solution39.java
@@ -12,6 +12,45 @@ import java.util.stream.Stream;
  *
  * Given an array of strings, group anagrams together.
  *
+ * other answers
+ *
+ *     Approach 1: Categorize by Sorted String
+ *
+ *     public List<List<String>> groupAnagrams(String[] strs) {
+ *         if (strs.length == 0) return new ArrayList();
+ *         Map<String, List> ans = new HashMap<String, List>();
+ *         for (String s : strs) {
+ *             char[] ca = s.toCharArray();
+ *             Arrays.sort(ca);
+ *             String key = String.valueOf(ca);
+ *             if (!ans.containsKey(key)) ans.put(key, new ArrayList());
+ *             ans.get(key).add(s);
+ *         }
+ *         return new ArrayList(ans.values());
+ *     }
+ *
+ *     Approach 2: Categorize by Count
+ *
+ *     public List<List<String>> groupAnagrams(String[] strs) {
+ *         if (strs.length == 0) return new ArrayList();
+ *         Map<String, List> ans = new HashMap<String, List>();
+ *         int[] count = new int[26];
+ *         for (String s : strs) {
+ *             Arrays.fill(count, 0);
+ *             for (char c : s.toCharArray()) count[c - 'a']++;
+ *
+ *             StringBuilder sb = new StringBuilder("");
+ *             for (int i = 0; i < 26; i++) {
+ *                 sb.append('#');
+ *                 sb.append(count[i]);
+ *             }
+ *             String key = sb.toString();
+ *             if (!ans.containsKey(key)) ans.put(key, new ArrayList());
+ *             ans.get(key).add(s);
+ *         }
+ *         return new ArrayList(ans.values());
+ *     }
+ *
  */
 class Solution39 {
     public List<List<String>> groupAnagrams(String[] strs) {
