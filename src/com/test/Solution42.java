78c716c0b58ffd70e6c0822d06fab35c799728c6
Kenny Oh <ohdoking@gmail.com>
2019-02-23 12:26:07 +0100

    fail Reverse node in K-group algorithms first .

---
+++ src/com/test/Solution42.java
@@ -0,0 +1,159 @@
+package com.test;
+
+import com.test.fundamental.ListNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Reverse Nodes in k-Group
+ *
+ * Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+ *
+ * k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
+ *
+ * Example:
+ *
+ * Given this linked list: 1->2->3->4->5
+ *
+ * For k = 2, you should return: 2->1->4->3->5
+ *
+ * For k = 3, you should return: 3->2->1->4->5
+ *
+ * Note:
+ *
+ * Only constant extra memory is allowed.
+ * You may not alter the values in the list's nodes, only nodes itself may be changed.
+ *
+ *
+ * i'm gonna explain how i can implement this code to solve.
+ *
+ * i'll walk through first before i solve this problem.
+ *
+ *   in this case
+ *   For k = 2, you should return: 2->1->4->3->5
+ *   while
+ *      nth = 1;
+ *      count = 0;
+ *          index 0((nth*k)-k) <-> 1((nth*k)-1) : 2->1->3->4->5
+ *          if count is same with (k/2)
+ *              break;
+ *      nth = 2;
+ *      count = 0;
+ *          index 2((nth*k)-k) <-> 3((nth*k)-1) : 2->1->4->3->5
+ *          if count is same with (k/2)
+ *              breka;
+ *      if specific index is null, finish this while loop.
+ *
+ *   now i'm gonna walk through other case
+ *
+ *   For k = 3, 1->2->3->4->5->6->7, you should return: 3->2->1->6->5->4->7;
+ *
+ *   while
+ *      nth = 1;
+ *      count = 0;
+ *          index 0(1 * 3 - 3) <-> 2(1 * 3 - 1) : 3->2->1->4->5->6->7;
+ *          if count is same with (k/2)
+ *              break;
+ *      nth = 2;
+ *      count = 0
+ *          index 3(2 * 3 - 3) <-> 5(2 * 3 - 1) : 3->2->1->6->5->4->7;
+ *          if count is same with (k/2)
+ *              break;
+ *
+ *      if specific index is null, finish this while loop.
+ *
+ *
+ */
+class Solution42 {
+    public ListNode reverseKGroup(ListNode head, int k) {
+/**
+ * For k = 3, 1->2->3->4->5->6->7, you should return: 3->2->1->6->5->4->7;
+ *  *
+ *  *   while
+ *  *      nth = 1;
+ *  *      count = 0;
+ *  *          index 0(1 * 3 - 3) <-> 2(1 * 3 - 1) : 3->2->1->4->5->6->7;
+ *  *          if count is same with (k/2)
+ *  *              break;
+ *  *      nth = 2;
+ *  *      count = 0
+ *  *          index 3(2 * 3 - 3) <-> 5(2 * 3 - 1) : 3->2->1->6->5->4->7;
+ *  *          if count is same with (k/2)
+ *  *              break;
+ *  *
+ *  *      if specific index is null, finish this while loop.
+ */
+        int nth = 1;
+        int count = 0;
+        boolean state = true;
+
+        Map<Integer, ListNode> map = new HashMap<>();
+        //set list node data to map
+        setMapFromListNode(head, map ,0);
+
+        while(state){
+            count = 0;
+            for(int i = 0 ; i < (k/2) ; i++){
+                // index 0((nth*k)-k) <-> 1((nth*k)-1) : 2->1->3->4->5
+                if(!swap(map, nth*k-k+count, nth*k-1-count)){
+                    state = false;
+                    break;
+                }
+                count++;
+            }
+        }
+
+        return head;
+    }
+
+    private void setMapFromListNode(ListNode head, Map<Integer, ListNode> map, int i) {
+        if(head == null){
+            return;
+        }
+        map.put(i, head);
+        setMapFromListNode(head.getNext(), map, i+1);
+    }
+
+    private boolean swap(Map<Integer, ListNode> map, int i, int i1) {
+        //if i1 is bigger than map size. we can't swap so this is wrong.
+        if(map.size() < i1){
+            return false;
+        }
+
+        if(i + 1 == i1){
+            ListNode beforeNode = map.get(i - 1);
+            ListNode node1 =  map.get(i);
+            ListNode node =  map.get(i1);
+            ListNode afterNode = map.get(i1 + 1);
+
+            if(i != 0){
+                beforeNode.setNext(node);
+            }
+            node1.setNext(node);
+            node.setNext(afterNode);
+
+        }
+        else{
+            ListNode beforeNode = map.get(i - 1);
+            ListNode node =  map.get(i1);
+            ListNode afterNode = map.get(i + 1);
+
+            if(i != 0){
+                beforeNode.setNext(node);
+            }
+            node.setNext(afterNode);
+
+            ListNode beforeNode1 = map.get(i1 - 1);
+            ListNode node1 =  map.get(i);
+            ListNode afterNode1 = map.get(i1 + 1);
+
+            beforeNode1.setNext(node1);
+            node1.setNext(afterNode1);
+        }
+
+
+        return true;
+    }
+
+}
---
+++ test/com/test/TestSolution42.java
@@ -0,0 +1,46 @@
+package com.test;
+
+import com.test.fundamental.ListNode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestSolution42 {
+
+    Solution42 solution42;
+
+    @BeforeEach
+    void setUp() {
+        solution42 = new Solution42();
+    }
+
+    @Test
+    public void test(){
+        //For k = 2, you should return: 2->1->4->3->5
+        ListNode listNode = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(3);
+        ListNode listNode4 = new ListNode(4);
+        ListNode listNode5 = new ListNode(5);
+        listNode4.setNext(listNode5);
+        listNode3.setNext(listNode4);
+        listNode2.setNext(listNode3);
+        listNode.setNext(listNode2);
+
+
+        ListNode listNode1 = new ListNode(2);
+        ListNode listNode1_2 = new ListNode(1);
+        ListNode listNode1_3 = new ListNode(4);
+        ListNode listNode1_4 = new ListNode(3);
+        ListNode listNode1_5 = new ListNode(5);
+        listNode1_4.setNext(listNode1_5);
+        listNode1_3.setNext(listNode1_4);
+        listNode1_2.setNext(listNode1_3);
+        listNode1.setNext(listNode1_2);
+
+        assertEquals(listNode1 ,solution42.reverseKGroup(listNode, 2));
+
+
+    }
+}
